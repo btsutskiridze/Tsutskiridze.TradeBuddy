588cb3ba2940107ee2acef84e8e5bd43cc257c88
btsutskirdze <baqaritsutskiridze222@gmail.com>
2025-03-09 22:42:35 +0400

    Add project files.

---
+++ Tsutskiridze.TradeBuddy/Program.cs
@@ -0,0 +1,40 @@
+using Microsoft.AspNetCore.Cors.Infrastructure;
+using Tsutskiridze.Bloom.Core;
+using Tsutskiridze.Bloom.Core.Common.Options;
+using Tsutskiridze.Bloom.Core.Secrets;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddBloom((opt) =>
+{
+    opt.AddMail = true;
+    opt.AddJobs = true;
+
+    opt.CorsOptions = new CorsConfOptions
+    {
+        ConfigurePolicy = (policy) =>
+        {
+            string[] origins = [.. (SecretsManager.GetSecret("Cors:AllowedOrigins").Split(','))];
+            policy.AddPolicy("AllowSpecificOrigins", delegate (CorsPolicyBuilder b)
+            {
+                b.WithOrigins(origins).AllowAnyHeader().AllowAnyMethod()
+                    .AllowCredentials();
+            });
+        }
+    };
+});
+
+var app = builder.Build();
+
+app.UseBloom();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.Run();
