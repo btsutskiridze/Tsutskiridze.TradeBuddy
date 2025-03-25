511a85cc1b08d85b8b84d3f36e1266e4ea6076a5
btsutskiridze <93125681+btsutskiridze@users.noreply.github.com>
2025-03-25 11:59:10 +0400

    ...

--- Tsutskiridze.TradeBuddy/Controllers/TelegramController.cs
+++ Tsutskiridze.TradeBuddy/Controllers/TelegramController.cs
@@ -1,8 +1,10 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿﻿using Microsoft.AspNetCore.Mvc;
 using Telegram.Bot;
 using Telegram.Bot.Types;
 using Telegram.Bot.Types.Enums;
 using Tsutskiridze.Bloom.Core.Common.Base;
+using Newtonsoft.Json;
+using System.Diagnostics;
 
 namespace Tsutskiridze.TradeBuddy.Controllers
 {
@@ -11,25 +13,39 @@ namespace Tsutskiridze.TradeBuddy.Controllers
     public class TelegramController : ApiControllerBase
     {
         private readonly ITelegramBotClient _botClient;
-
-        public TelegramController(ITelegramBotClient botClient)
+        private readonly ILogger<TelegramController> _logger;
+        
+        public TelegramController(ITelegramBotClient botClient, ILogger<TelegramController> logger)
         {
             _botClient = botClient;
+            _logger = logger;
         }
 
         [HttpPost("webhook")]
         public async Task<IActionResult> Webhook([FromBody] Update update)
         {
-            if (update == null)
+            
+            _logger.LogInformation("request:");
+            _logger.LogInformation(JsonConvert.SerializeObject(update));
+            
+            
+            if (update == null){
+                _logger.LogError("bad_request");
                 return BadRequest();
-
-            if (update.Type == UpdateType.Message)
-            {
-                var message = update.Message;
-                await _botClient.SendMessage(message.Chat.Id, $"You said: {message.Text}");
             }
 
-            return Ok();
+            try{
+                if (update.Type == UpdateType.Message)
+                {
+                    var message = update.Message;
+                    _logger.LogInformation("updated");
+                    await _botClient.SendMessage(message.Chat.Id, $"You said: {message.Text}");
+                }
+
+                return Ok();
+            }catch (Exception ex){
+                throw;
+            }
         }
     }
 }
