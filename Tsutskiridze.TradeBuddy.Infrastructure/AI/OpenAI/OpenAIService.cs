30978cf586a89aa8637bbc0dff24c835b32c6fe7
btsutskiridze <93125681+btsutskiridze@users.noreply.github.com>
2025-04-30 14:47:06 +0400

    Update OpenAIService.cs

--- Tsutskiridze.TradeBuddy.Infrastructure/AI/OpenAI/OpenAIService.cs
+++ Tsutskiridze.TradeBuddy.Infrastructure/AI/OpenAI/OpenAIService.cs
@@ -24,7 +24,7 @@ namespace Tsutskiridze.TradeBuddy.Infrastructure.AI.OpenAI
             ChatCompletionOptions options = new()
             {
                 MaxOutputTokenCount = 500,
-                Temperature = 1,
+                Temperature = 0.2,
                 ResponseFormat = ChatResponseFormat.CreateJsonSchemaFormat(
                     jsonSchemaFormatName: "stock_analysis",
                     jsonSchema: BinaryData.FromString(OpenaiJsSchemaGenerator.FromType(typeof(T))),
