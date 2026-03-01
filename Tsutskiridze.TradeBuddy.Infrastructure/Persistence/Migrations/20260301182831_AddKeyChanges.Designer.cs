3d3064b05f6fe42e9684c70a6c0bec2d7116fa91
btsutskirdze <baqaritsutskiridze222@gmail.com>
2026-03-01 22:30:46 +0400

    feat: update migrations

--- Tsutskiridze.TradeBuddy.Infrastructure/Persistence/Migrations/20260301182831_AddKeyChanges.Designer.cs
+++ Tsutskiridze.TradeBuddy.Infrastructure/Persistence/Migrations/20260301182831_AddKeyChanges.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using Tsutskiridze.TradeBuddy.Infrastructure.Persistence;
@@ -11,9 +12,11 @@ using Tsutskiridze.TradeBuddy.Infrastructure.Persistence;
 namespace Tsutskiridze.TradeBuddy.Infrastructure.Persistence.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260301182831_AddKeyChanges")]
+    partial class AddKeyChanges
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
