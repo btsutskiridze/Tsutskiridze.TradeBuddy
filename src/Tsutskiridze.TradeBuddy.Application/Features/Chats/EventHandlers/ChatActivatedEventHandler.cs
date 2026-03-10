ad08e758ea33fa0cfb2b500ed6869f9315d1ffbd
btsutskirdze <baqaritsutskiridze222@gmail.com>
2026-03-11 01:05:04 +0400

    feat: chatActivatedEventHandler

---
+++ src/Tsutskiridze.TradeBuddy.Application/Features/Chats/EventHandlers/ChatActivatedEventHandler.cs
@@ -0,0 +1,21 @@
+﻿using Mediator;
+using SharedKernel;
+using Tsutskiridze.TradeBuddy.Application.Abstractions.Notifications.Telegram;
+using Tsutskiridze.TradeBuddy.Core.Aggregates.Chats.Events;
+
+namespace Tsutskiridze.TradeBuddy.Application.Features.Chats.EventHandlers;
+
+public sealed class ChatActivatedEventHandler : INotificationHandler<ChatActivatedEvent>
+{
+    private readonly ITelegramSender _sender;
+
+    public ChatActivatedEventHandler(ITelegramSender sender)
+    {
+        _sender = sender;
+    }
+
+    public async ValueTask Handle(ChatActivatedEvent notification, CancellationToken cancellationToken)
+    {
+        await _sender.SendMessage(notification.TelegramChatId, "StockBuddy Activated Successfully", cancellationToken);
+    }
+}
