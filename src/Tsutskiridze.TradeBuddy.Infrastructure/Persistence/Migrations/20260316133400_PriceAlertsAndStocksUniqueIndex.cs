d3af8a2513a1cc1721f80022d01b140ceee8e126
btsutskirdze <baqaritsutskiridze222@gmail.com>
2026-03-16 17:35:20 +0400

    refactor: updated migrations

---
+++ src/Tsutskiridze.TradeBuddy.Infrastructure/Persistence/Migrations/20260316133400_PriceAlertsAndStocksUniqueIndex.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Tsutskiridze.TradeBuddy.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class PriceAlertsAndStocksUniqueIndex : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "ix_price_alerts_chat_id",
+                table: "price_alerts");
+
+            migrationBuilder.CreateIndex(
+                name: "UX_stocks_symbol",
+                table: "stocks",
+                column: "symbol",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "UX_price_alerts_chat_stock_direction_price",
+                table: "price_alerts",
+                columns: new[] { "chat_id", "stock_id", "direction", "price" },
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "UX_stocks_symbol",
+                table: "stocks");
+
+            migrationBuilder.DropIndex(
+                name: "UX_price_alerts_chat_stock_direction_price",
+                table: "price_alerts");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_price_alerts_chat_id",
+                table: "price_alerts",
+                column: "chat_id");
+        }
+    }
+}
