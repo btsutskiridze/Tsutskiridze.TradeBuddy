ca4e61e4a01e4ebd1f5f1ed01f92d822d55e66eb
btsutskirdze <baqaritsutskiridze222@gmail.com>
2026-04-14 01:30:44 +0400

    refactor: reorganize controllers for stocks and update routes

---
+++ src/Tsutskiridze.TradeBuddy.API/Controllers/Stocks/AnalysisController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Tsutskiridze.TradeBuddy.Application.Features.StockAnalysis.Services;
+
+namespace Tsutskiridze.TradeBuddy.API.Controllers.Stocks
+{
+    [ApiController]
+    [Route("api/stocks/{symbol}/analysis")]
+    public class AnalysisController : ControllerBase
+    {
+        private readonly StockAnalysisGenerator _stockAnalysisGenerator;
+
+        public AnalysisController(StockAnalysisGenerator stockAnalysisGenerator)
+        {
+            _stockAnalysisGenerator = stockAnalysisGenerator;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Analyze([FromRoute] string symbol)
+        {
+            var result = await _stockAnalysisGenerator.AnalyzeAsync(symbol);
+
+            if (result == null)
+            {
+                return BadRequest("Stock analysis returned null");
+            }
+
+            return Ok(result);
+        }
+    }
+}
--- src/Tsutskiridze.TradeBuddy.API/Controllers/Stocks/NewsController.cs
+++ src/Tsutskiridze.TradeBuddy.API/Controllers/Stocks/NewsController.cs
@@ -1,4 +1,4 @@
-using Mediator;
+﻿using Mediator;
 using Microsoft.AspNetCore.Mvc;
 using Tsutskiridze.TradeBuddy.Application.Enums;
 using Tsutskiridze.TradeBuddy.Application.Features.News.Queries;
@@ -7,34 +7,34 @@ using Tsutskiridze.TradeBuddy.Application.Features.News.Queries.Google;
 using Tsutskiridze.TradeBuddy.Application.Features.News.Queries.Reddit;
 using Tsutskiridze.TradeBuddy.Application.Features.News.Queries.Yahoo;
 
-
-namespace Tsutskiridze.TradeBuddy.API.Controllers
+namespace Tsutskiridze.TradeBuddy.API.Controllers.Stocks
 {
     [ApiController]
-    [Route("api/news")]
-    public class StockNewsController : ControllerBase
+    [Route("api/stocks/{symbol}/news")]
+    public class NewsController : ControllerBase
     {
         private readonly IMediator _mediator;
 
-        public StockNewsController(IMediator mediator)
+        public NewsController(IMediator mediator)
         {
             _mediator = mediator;
         }
 
-        [HttpGet("{symbol}")]
-        public async Task<IActionResult> GetStockNews(string symbol, [FromQuery] int limit = 10)
+        [HttpGet]
+        public async Task<IActionResult> Get([FromRoute] string symbol, [FromQuery] int limit = 10)
         {
             var result = await _mediator.Send(new StockNewsQuery(symbol, limit));
 
             return Ok(result);
         }
 
-        // Reverse the uris for concrete sources:
-        [HttpGet("{symbol}/reddit")]
-        public async Task<IActionResult> GetRedditPosts(string symbol, [FromQuery] int limit = 10,
+        [HttpGet("reddit")]
+        public async Task<IActionResult> GetReddit(
+            [FromRoute] string symbol,
+            [FromQuery] int limit = 10,
             [FromQuery] string sortType = "new")
         {
-            RedditSortType sort = Enum.TryParse<RedditSortType>(sortType, true, out var sortTypeEnum)
+            var sort = Enum.TryParse<RedditSortType>(sortType, true, out var sortTypeEnum)
                 ? sortTypeEnum
                 : RedditSortType.New;
 
@@ -43,29 +43,28 @@ namespace Tsutskiridze.TradeBuddy.API.Controllers
             return Ok(posts);
         }
 
-        [HttpGet("{symbol}/google")]
-        public async Task<IActionResult> GetGoogleNews(string symbol, [FromQuery] int limit = 10)
+        [HttpGet("google")]
+        public async Task<IActionResult> GetGoogle([FromRoute] string symbol, [FromQuery] int limit = 10)
         {
             var news = await _mediator.Send(new GoogleNewsQuery(symbol, limit));
 
             return Ok(news);
         }
 
-        [HttpGet("{symbol}/yahoo")]
-        public async Task<IActionResult> GetYahooNews(string symbol, [FromQuery] int limit = 10)
+        [HttpGet("yahoo")]
+        public async Task<IActionResult> GetYahoo([FromRoute] string symbol, [FromQuery] int limit = 10)
         {
             var news = await _mediator.Send(new YahooNewsQuery(symbol, limit));
 
             return Ok(news);
         }
 
-        [HttpGet("{symbol}/finnhub")]
-        public async Task<IActionResult> GetFinnhubNews(
-            string symbol,
+        [HttpGet("finnhub")]
+        public async Task<IActionResult> GetFinnhub(
+            [FromRoute] string symbol,
             [FromQuery] DateTime from,
             [FromQuery] DateTime to,
-            [FromQuery] int limit = 10
-        )
+            [FromQuery] int limit = 10)
         {
             var news = await _mediator.Send(new FinnhubNewsQuery(symbol, from, to, limit));
 
