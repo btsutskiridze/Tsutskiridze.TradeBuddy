8120e088a6d250183494739f02da74b85c6d1d04
btsutskirdze <baqaritsutskiridze222@gmail.com>
2026-04-16 16:10:00 +0400

    fix: exceptions

--- src/Tsutskiridze.TradeBuddy.Application/Exceptions/ApplicationLayerException.cs
+++ src/Tsutskiridze.TradeBuddy.Application/Exceptions/ApplicationLayerException.cs
@@ -4,7 +4,7 @@ namespace Tsutskiridze.TradeBuddy.Application.Exceptions;
 
 public class ApplicationLayerException : BaseException
 {
-    public ApplicationLayerException(string message, int statusCode = 400) : base(message, statusCode)
+    public ApplicationLayerException(string message, int statusCode = 400, Exception? inner = null) : base(message, statusCode, inner)
     {
     }
 }
--- src/Tsutskiridze.TradeBuddy.Application/Exceptions/ConcurrencyConflictException.cs
+++ src/Tsutskiridze.TradeBuddy.Application/Exceptions/ConcurrencyConflictException.cs
@@ -1,9 +1,9 @@
 ﻿namespace Tsutskiridze.TradeBuddy.Application.Exceptions;
 
-public sealed class ConcurrencyConflictException : Exception
+public sealed class ConcurrencyConflictException : ApplicationLayerException
 {
     public ConcurrencyConflictException(string message, Exception? inner = null)
-        : base(message, inner)
+        : base(message, 409, inner)
     {
     }
 }
