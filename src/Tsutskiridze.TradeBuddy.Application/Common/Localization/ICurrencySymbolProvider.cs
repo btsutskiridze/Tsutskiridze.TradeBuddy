71366b64752af530f9efe4a3439b5324615e122c
btsutskirdze <baqaritsutskiridze222@gmail.com>
2026-04-25 02:14:23 +0400

    chore: remove comment

--- src/Tsutskiridze.TradeBuddy.Application/Common/Localization/ICurrencySymbolProvider.cs
+++ src/Tsutskiridze.TradeBuddy.Application/Common/Localization/ICurrencySymbolProvider.cs
@@ -1,6 +1,5 @@
 namespace Tsutskiridze.TradeBuddy.Application.Common.Localization
 {
-    //todo: refactor this abstractions too
     public interface ICurrencySymbolProvider
     {
         string? GetSymbol(string currencyCode);
